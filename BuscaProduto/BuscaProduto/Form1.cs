12d944082d76ea6e21019030df142ad2eb51c6e9
Marcelot <marcelojtelles@gmail.com>
2019-04-06 20:18:27 -0300

    Add files via upload

---
+++ BuscaProduto/BuscaProduto/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BuscaProduto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("clnName","Nome");
+            
+            try
+            {
+                string[] linhas = System.IO.File.ReadAllLines("..\\..\\produtos.txt");
+                Array.Sort(linhas);
+                foreach (string l in linhas)
+                {
+                    dataGridView1.Rows.Add(l);
+                }
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Falha na abertura do arquivo produtos.txt");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            try
+            {
+                List<Produto> produtos = new List<Produto>();
+                string[] linhas = System.IO.File.ReadAllLines("..\\..\\produtos2.txt");
+                Array.Sort(linhas);
+                foreach (string l in linhas)
+                {
+
+                    string[] np = l.Split(';');
+                    Produto p = new Produto(np[0],float.Parse(np[1]));
+                    produtos.Add(p);
+                    
+                }
+                dataGridView1.DataSource = produtos;
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Falha na abertura do arquivo produtos.txt");
+            }
+        }
+    }
+}
---
+++ BuscaProduto/BuscaProduto/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuscaProduto
+{
+    class Produto
+    {
+       public string nomeProduto {get; set; }
+       public float precoProduto { get; set; }
+       public Produto(string n, float p) {
+           nomeProduto = n;
+           precoProduto = p;
+       }
+
+    }
+}
